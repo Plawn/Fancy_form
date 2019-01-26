2259619e0a569c3bfe366c803dec9ca28f8f22f8
Plawn <plawn.yay@gmail.com>
2019-01-26 21:25:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const c_div = document.getElementById('inscription_container');
 const f = new Form(c_div);
 
 
-f.load_from_JSON({ name: 'test', deb: 'ja', mail: 'paul.leveau@isep.fr' }, { label: true, labelize: k => '1' + k });
+f.load_from_JSON({ name: 'test', deb: 'ja'}, { label: true, labelize: k => '1' + k });
 
 f.get_named('mail').add_checker(new Checker([new RuleLength(1, 200), new RuleMatchRegExp(reg_mail)]));
 
